7de9ca0c9b420dc84549877e6ed52dc5a73f3df1
Yann3D <yannick.jouneau3d@free.fr>
2016-11-12 04:52:21 +0100

    Death + Checkpoint

---
+++ Assets/Scripts/Death.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(BoxCollider))]
+
+public class Death : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		GetComponent<BoxCollider>().isTrigger = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter (Collider collider){
+		if (collider.CompareTag("Player")){
+			//collider.GetComponent<GoblinController>().Respawn();
+		}
+	}
+}
