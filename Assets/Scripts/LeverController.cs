abc9e860a7ac6105d0bb5bf82d5126fe80839f4f
Mulverick <frederic.dubourg@epitech.eu>
2016-11-12 05:42:41 +0100

    Levier et activables

---
+++ Assets/Scripts/LeverController.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Collider))]
+public class LeverController : MonoBehaviour
+{
+    public GameObject activable;
+    public float time = 3f;
+    public bool isTimed = false;
+    private float timer = 0f;
+    private bool state = false;
+    private bool triggered = false;
+
+    void Update()
+    {
+        if (triggered)
+        {
+            if (Input.GetButtonDown("Fire1"))
+            {
+                if (!state)
+                {
+                    activable.GetComponent<Animator>().SetTrigger("Open");
+                    state = true;
+                }
+            }
+        }
+        if (state && isTimed)
+        {
+            timer += Time.deltaTime;
+            if (timer >= time)
+            {
+                timer = 0;
+                activable.GetComponent<Animator>().SetTrigger("Close");
+                state = false;
+            }
+        }
+    }
+
+
+
+    void OnTriggerExit(Collider other)
+    {
+        triggered = false;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        triggered = true;
+    }
+}
