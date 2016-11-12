27a79d4a3f9ee9039e4d3bf649bfa3328158b4d2
thebatoust <thebatoust@gmail.com>
2016-11-12 15:43:12 +0100

    Mouvement 2P + spots photo

---
+++ Assets/Scripts/PressurePlateController.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Collider))]
+public class PressurePlateController : MonoBehaviour {
+
+    public GameObject[] activables;
+    public GameObject movablesRoot;
+    public float time = 3f;
+    public bool isTimed = false;
+    private float timer = 0f;
+    private bool state = false;
+    private bool triggered = false;
+
+    void Update()
+    {
+        if (triggered)
+        {
+            if (!state)
+            {
+                foreach (GameObject activable in activables)
+                {
+                    activable.GetComponent<Animator>().SetTrigger("Open");
+                }
+                state = true;
+            }
+        }
+        if (state && isTimed)
+        {
+            timer += Time.deltaTime;
+            if (timer >= time)
+            {
+                timer = 0;
+                foreach (GameObject activable in activables)
+                {
+                    activable.GetComponent<Animator>().SetTrigger("Close");
+                }
+                state = false;
+            }
+        }
+    }
+
+
+
+    void OnTriggerExit(Collider other)
+    {
+        triggered = false;
+        movablesRoot.GetComponent<Animator>().SetTrigger("Close");
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        triggered = true;
+        movablesRoot.GetComponent<Animator>().SetTrigger("Open");
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        triggered = true;
+        movablesRoot.GetComponent<Animator>().SetTrigger("Open");
+    }
+}
