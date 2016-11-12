d60aabe7fa04d972317f9211db3a898bf64aa54c
Mulverick <frederic.dubourg@epitech.eu>
2016-11-12 19:16:32 +0100

    Indicateurs position goblin

---
+++ Assets/FollowIndicator.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class FollowIndicator : MonoBehaviour {
+
+    GameObject Indicator;
+
+	// Use this for initialization
+	void Start ()
+    {
+	
+	}
+
+    void LateUpdate()
+    {
+        if (Indicator == null)
+        {
+            Indicator = GameObject.Find("Indicator");
+            return;
+        }
+        if (Indicator.activeSelf && this.transform.parent != Indicator.transform)
+        {
+            this.transform.SetParent(Indicator.transform, false);
+            transform.localPosition += new Vector3(0, -5, 0);
+            transform.GetComponent<RectTransform>().rotation.SetEulerAngles(new Vector3(0, 0, -Indicator.GetComponent<RectTransform>().rotation.eulerAngles.z));
+            Indicator.GetComponent<Image>().preserveAspect = true;
+        }
+        else if (Indicator.transform == transform.parent)
+        {
+            GetComponent<Image>().color = Indicator.GetComponent<Image>().color;
+        }
+
+    }
+}
