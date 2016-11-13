028c0619c2c6b24d9f00877d0c7e28e92a11988b
thebatoust <thebatoust@gmail.com>
2016-11-13 13:30:13 +0100

    FINI CE FUCKING PUTAIN DE MENU PRINCIPAL DE SES MORTS NTM

--- Assets/Scripts/MainMenuManager.cs
+++ Assets/Scripts/MainMenuManager.cs
@@ -3,14 +3,46 @@ using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
 using DG.Tweening;
+using UnityEngine.SceneManagement;
 
 public class MainMenuManager : MonoBehaviour
 {
     [SerializeField]
     private Selectable firstSelected;
+
+    [SerializeField]
+    private Selectable creditsFirstSelected;
+
+    public CanvasGroup creditsPanel;
     
     void Start()
     {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
         firstSelected.Select();
+    }
+
+    public void DisplayCredits(bool display)
+    {
+        if (display) {
+            creditsPanel.DOFade(1f, 0.6f);
+            creditsPanel.blocksRaycasts = true;
+            creditsFirstSelected.Select();
+        } else
+        {
+            firstSelected.Select();
+            creditsPanel.DOFade(0f, 0.6f);
+            creditsPanel.blocksRaycasts = false;
+        }
+    }
+
+    public void LaunchGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
     }
 }
