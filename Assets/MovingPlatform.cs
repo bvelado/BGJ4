de81c3f47ebdf131acce6d9224b7708fd0b178d8
thebatoust <thebatoust@gmail.com>
2016-11-13 19:45:05 +0100

    lel

--- Assets/MovingPlatform.cs
+++ Assets/MovingPlatform.cs
@@ -2,10 +2,28 @@
 using System.Collections;
 
 public class MovingPlatform : MonoBehaviour {
-    
-    void OnColliderEnter(Collider col)
-    {
 
+    public Transform oldParent;
+
+    void OnCollisionEnter(Collision col)
+    {
+        print("test");
+
+        if(col.gameObject.CompareTag("Orc"))
+        {
+            oldParent = col.transform;
+            col.transform.SetParent(transform, true);
+        }
+    }
+
+    void OnCollisionExit(Collision col)
+    {
+        print("nop");
+
+        if (col.gameObject.CompareTag("Orc"))
+        {
+            col.transform.SetParent(null);
+        }
     }
 
 }
