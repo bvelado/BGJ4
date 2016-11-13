674e9eae8bbcd37ef37408a653f71644514351ec
thebatoust <thebatoust@gmail.com>
2016-11-13 10:27:42 +0100

    Level 1

---
+++ Assets/Lever.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class Lever : MonoBehaviour, IUsable
+{
+    private Animator m_animator;
+
+    void Start()
+    {
+        m_animator = GetComponent<Animator>();
+    }
+
+    public void Use()
+    {
+        m_animator.SetTrigger("Activate");
+    }
+}
